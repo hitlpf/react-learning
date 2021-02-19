f620fb12afab26ee1a0e2ecde099866c0bf93143
liupengfei <liupengfei@sogou-inc.com>
2021-02-19 14:26:06 +0800

    feat: 代码修改

--- src/server/app.js
+++ src/server/app.js
@@ -6,6 +6,7 @@ const assets = require('koa-static');
 const app = new Koa();
 
 const router = require('../router/serverRouter');
+const router2 = require('../router/serverRouter');
 
 //koa-views设置文件路径和文件类型(路径views一定要和项目中页面文件路径一致)
 //可以是任何类型的文件：html，也可以是模块文件（pug或nunjucks），nunjucks使用时不使用koa-views模块而是使用koa-nunjucks-2模块
