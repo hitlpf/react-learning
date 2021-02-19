1d5798dd13743d84527bfd60079c7eab9e801eae
liupengfei <liupengfei@sogou-inc.com>
2021-02-19 14:43:46 +0800

    feat: 代码更新

--- src/app/main.js
+++ src/app/main.js
@@ -32,10 +32,10 @@
 
 
 
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './components/App';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App'
 
 render((
   <BrowserRouter>
